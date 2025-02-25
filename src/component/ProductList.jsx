386394223d16a775de51172d443052f2d4bd48dc
Samuel Narvaja Torres <samuel.narvaja.torres@gmail.com>
2025-02-25 17:15:50 -0300

    change style button cerrar sesion y agregar producto

--- src/component/ProductList.jsx
+++ src/component/ProductList.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { supabase } from '../utils/supabaseClient';
-import ProductCard from './ProductCard';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../utils/supabaseClient";
+import ProductCard from "./ProductCard";
 // eslint-disable-next-line react/prop-types
-const ProductList = ({isAdmin,onLogout}) => {
+const ProductList = ({ isAdmin, onLogout }) => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
@@ -11,7 +11,7 @@ const ProductList = ({isAdmin,onLogout}) => {
   useEffect(() => {
     const fetchProducts = async () => {
       setLoading(true);
-      const { data, error } = await supabase.from('products').select('*');
+      const { data, error } = await supabase.from("products").select("*");
       if (error) console.error("Error fetching products:", error);
       else setProducts(data);
       setLoading(false);
@@ -19,51 +19,55 @@ const ProductList = ({isAdmin,onLogout}) => {
     fetchProducts();
   }, []);
 
-
   const deleteProduct = async (productId, image) => {
     try {
       if (image) {
         // Si la imagen está en products/images/
         // Necesitamos extraer solo la parte del path después de 'products/'
-        
-        console.log('URL completa de la imagen:', image);
-        
+
+        console.log("URL completa de la imagen:", image);
+
         // Verificar si la URL contiene el patrón esperado
-        if (image.includes('products/images/')) {
-          // Extraer el nombre del archivo 
-          const fileName = image.split('/').pop();
+        if (image.includes("products/images/")) {
+          // Extraer el nombre del archivo
+          const fileName = image.split("/").pop();
           // La ruta dentro del bucket debería ser 'images/nombredelarchivo'
           const filePath = `images/${fileName}`;
-          
-          console.log('Ruta para eliminar:', filePath);
-          
+
+          console.log("Ruta para eliminar:", filePath);
+
           const { error: deleteImageError } = await supabase.storage
-            .from('products')
+            .from("products")
             .remove([filePath]);
-          
+
           if (deleteImageError) {
-            console.error('Error eliminando la imagen del bucket: ', deleteImageError);
+            console.error(
+              "Error eliminando la imagen del bucket: ",
+              deleteImageError
+            );
             return;
           }
         } else {
-          console.error('Formato de URL de imagen no reconocido:', image);
+          console.error("Formato de URL de imagen no reconocido:", image);
           return;
         }
       }
-  
-      const { error } = await supabase.from('products').delete().eq('id', productId);
+
+      const { error } = await supabase
+        .from("products")
+        .delete()
+        .eq("id", productId);
       if (error) {
-        console.error('Error eliminando el producto: ', error);
+        console.error("Error eliminando el producto: ", error);
         return;
       } else {
         // Actualiza el estado para eliminar el producto de la lista
-        setProducts(products.filter(product => product.id !== productId));
+        setProducts(products.filter((product) => product.id !== productId));
       }
     } catch (error) {
-      console.error('Error al eliminar el producto o la imagen: ', error);
+      console.error("Error al eliminar el producto o la imagen: ", error);
     }
   };
-  
 
   return (
     <div className="container mx-auto px-4 py-8">
@@ -72,19 +76,45 @@ const ProductList = ({isAdmin,onLogout}) => {
         <div className="flex gap-2">
           {isAdmin ? (
             <>
-              <button onClick={() => navigate('/add-product')} className="px-4 py-2 bg-green-500 text-white rounded">Agregar Producto</button>
-              <button onClick={onLogout} className="px-4 py-2 bg-red-500 text-white rounded">Cerrar Sesión</button>
+              <button
+                onClick={() => navigate("/add-product")}
+                className="px-6 py-2 text-sm bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 transition"
+              >
+                Agregar Producto
+              </button>
+              <button
+                onClick={onLogout}
+                className="px-6 py-2 text-sm bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition"
+              >
+                Cerrar Sesión
+              </button>
             </>
           ) : (
-            <button onClick={() => navigate('/login')} className="px-4 py-2 bg-blue-500 text-white rounded">Admin Login</button>
+            <button
+              onClick={() => navigate("/login")}
+              className="px-4 py-2 bg-blue-500 text-white rounded"
+            >
+              Admin Login
+            </button>
           )}
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {loading ? <p>Cargando productos...</p> : products.map(product => <ProductCard key={product.id} product={product} isAdmin={isAdmin} onDelete={() => deleteProduct(product.id,product.image)} />)}
+        {loading ? (
+          <p>Cargando productos...</p>
+        ) : (
+          products.map((product) => (
+            <ProductCard
+              key={product.id}
+              product={product}
+              isAdmin={isAdmin}
+              onDelete={() => deleteProduct(product.id, product.image)}
+            />
+          ))
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default ProductList
+export default ProductList;
